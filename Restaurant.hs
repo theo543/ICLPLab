edb4a5b2ba20c7cc4ab9c08b12ebccb84b940e91
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-11-22 10:19:40 +0200

    Add restaurant exit signals, main action

--- Restaurant.hs
+++ Restaurant.hs
@@ -1,5 +1,5 @@
-import Control.Concurrent (MVar, takeMVar, putMVar, threadDelay)
-import Control.Monad (forever)
+import Control.Concurrent (MVar, takeMVar, putMVar, newMVar, newEmptyMVar, forkIO, threadDelay)
+import Control.Monad (forever, void, replicateM)
 import Data.Maybe (fromJust)
 import Data.List (elemIndex)
 data Seat = Free | Taken deriving (Eq, Show)
@@ -28,8 +28,8 @@ freeSeat = updateSeat Free
 stdoPrint :: MVar String -> IO ()
 stdoPrint stdo = forever $ takeMVar stdo >>= putStrLn
 
-client :: MVar String -> MRestaurant -> Int -> IO ()
-client stdo restaurant clientIdx = do
+client :: MVar () -> MVar String -> MRestaurant -> Int -> IO ()
+client exitSignal stdo restaurant clientIdx = do
     (seats, queue) <- takeMVar restaurant
     if full seats then do
         putMVar restaurant (seats, queue ++ [clientIdx])
@@ -39,17 +39,30 @@ client stdo restaurant clientIdx = do
         let newSeats = takeSeat seatIdx seats
         putMVar restaurant (newSeats, queue)
         clientIn stdo restaurant clientIdx seatIdx
+    putMVar exitSignal ()
 
 clientIn :: MVar String -> MRestaurant -> Int -> Int -> IO ()
 clientIn stdo restaurant clientIdx seatIdx = do
     putMVar stdo $ "Client " ++ show clientIdx ++ " in seat " ++ show seatIdx ++ "."
-    threadDelay 5_000_000
+    threadDelay 2_000_000
     putMVar stdo $ "Client " ++ show clientIdx ++ " leaving restaurant."
     (seats, queue) <- takeMVar restaurant
     case queue of
-        [] -> putMVar restaurant (freeSeat seatIdx seats, queue)
+        [] -> do
+            putMVar restaurant (freeSeat seatIdx seats, queue)
         nextClientIdx : queueTail -> do
             putMVar restaurant (seats, queueTail)
-            clientIn stdo restaurant  nextClientIdx seatIdx
+            clientIn stdo restaurant nextClientIdx seatIdx
 
--- TODO TEST THIS
+clients :: Int
+clients = 10
+
+main :: IO ()
+main = do
+    stdo <- newEmptyMVar
+    forkIO $ stdoPrint stdo
+
+    restaurant <- newMVar ([Free, Free, Free], [])
+    exitSignals <- replicateM clients newEmptyMVar
+    mapM_ (\(exSignal, cIdx) -> forkIO $ client exSignal stdo restaurant cIdx) $ zip exitSignals [1..clients]
+    mapM_ takeMVar exitSignals
